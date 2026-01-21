8defd5a93fcc01d2b6d22915d202b0f8136edf4f
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-01-21 11:31:45 -0500

    Add header and footer with basic navigation

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+
+    </main>
+    <footer>
+        <p>Copyright © 2026 - JF. All rights reserved</p>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Movie Manager</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Movie Manager</h1>
+        <nav>
+            <a href="index.php" class="nav-start">Start</a>
+            <a href="add_movie.php" class="add-movie">Add Movie</a>
+        </nav>
+    </header>
+    <main>
